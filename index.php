50d0e0430f449fbca4c19c5ae685007afe2f07f8
DauberSide <72296037+dauberside@users.noreply.github.com>
2020-10-04 01:32:46 +0900

    Update index.php

--- index.php
+++ index.php
@@ -14,3 +14,30 @@ function escape($str) {
     return htmlspecialchars($str,ENT_QUOTES,'UTF-8');
 }
 ?>
+<html>
+    <body>
+           <!-- content goes here -->
+        <div class="form-area">  
+            <form role="form">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Name" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="email" name="email" placeholder="Email" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="mobile" name="mobile" placeholder="Mobile Number" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" required>
+                        </div>
+                        <div class="form-group">
+                        <textarea class="form-control" type="textarea" id="message" placeholder="Message" maxlength="140" rows="7"></textarea>
+                            <span class="help-block"><p id="characterLeft" class="help-block ">You have reached the limit</p></span>                    
+                        </div>
+            </form>
+        </div>
+         <!-- content goes here end-->
+    </body>
+</html>
+   
