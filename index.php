47497914e079d2c2b224252be7c5817742c89b09
DauberSide <72296037+dauberside@users.noreply.github.com>
2020-10-04 01:25:50 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php phpinfo();
+<?php
+session_start();
+
+//クリックジャッキング対策
+header('X-FRAME-OPTIONS: SAMEORIGIN');
+
+// トークン生成
+if (!isset($_SESSION['token'])) {
+    $_SESSION['token'] = sha1(random_bytes(30));
+}
+
+// HTML特殊文字をエスケープする関数
+function escape($str) {
+    return htmlspecialchars($str,ENT_QUOTES,'UTF-8');
+}
+?>
