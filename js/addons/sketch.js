3e3e2188059887d9f1872f3fab38407177af2249
dauberside <72296037+dauberside@users.noreply.github.com>
2020-12-16 11:57:01 +0900

    Update sketch.js

--- js/addons/sketch.js
+++ js/addons/sketch.js
@@ -1,13 +1,25 @@
+const { LINE_LOOP } = require("../p5");
+
 var canvas;
+
+function windowResized() {
+  //console.log('resized');//
+  resizeCanvas(windowWidth, windHeight);
+}
+
+
 
 function setup() {
   // put setup code here
   canvas = createCanvas(windowWidth, windHeight);
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
+  background(175);
 }
 
 function draw() {
   // put drawing code here
-  background(175);
+  if (mouseisPressed) {
+    line(pmouseX, pmouseY, mouseX, mouseY);
+  }
 }
