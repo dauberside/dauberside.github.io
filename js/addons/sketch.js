d3b012141bd6443cc667ea8ad192f513750ff4f3
dauberside <72296037+dauberside@users.noreply.github.com>
2020-12-16 11:45:53 +0900

    Update sketch.js

--- js/addons/sketch.js
+++ js/addons/sketch.js
@@ -2,12 +2,12 @@ var canvas;
 
 function setup() {
   // put setup code here
-  createCanvas(400, 400);
+  canvas = createCanvas(windowWidth, windHeight);
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
 }
 
 function draw() {
   // put drawing code here
-  background(0);
+  background(175);
 }
