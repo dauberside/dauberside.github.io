7be7b9222474d1aab9e37cf3b92ba5b09dd182c0
dauberside <72296037+dauberside@users.noreply.github.com>
2020-12-17 20:57:53 +0900

    Update sketch.js

--- js/addons/sketch.js
+++ js/addons/sketch.js
@@ -34,17 +34,18 @@ function windowResized() {
 
 function setup() {
   // put setup code here
-  canvas = createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(displayWidth, displayHeight);
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
   background('rgba(2%,20%,255%,0.1)');
+  strokeWeight(10);
+  storke(0)
 }
 
 function draw() {
   // put drawing code here
   if (mouseIsPressed) {
-    strokeWeight(10);
-    //line(pmouseX, pmouseY, mouseX, mouseY);
+    line(pmouseX, pmouseY, mouseX, mouseY);
   }
   spray();
 }
@@ -52,8 +53,8 @@ function draw() {
 function spray() {
   if (mouseIsPressed) {
     for (let i = 0; i < 400; i++) {
-      let xc = constrain(pmouseX, mouseX, windowWidth);
-      let yc = constrain(pmouseY, mouseY, windowHeight);
+      let xc = constrain(pmouseX, mouseX, displayWidth);
+      let yc = constrain(pmouseY, mouseY, displayHeight);
       let x = random(-15, 15);
       let y = random(-15, 15);
       if (dist(0, 0, x, y) < 10) {
