1ab40e4dbba0aba728dd00bcc0346f83efa903a7
dauberside <72296037+dauberside@users.noreply.github.com>
2020-12-17 20:45:37 +0900

    Update sketch.js

--- js/addons/sketch.js
+++ js/addons/sketch.js
@@ -23,6 +23,47 @@
 //   }
 // }
 
+// var canvas;
+
+// function windowResized() {
+//   //console.log('resized');//
+//   resizeCanvas(windowWidth, windowHeight);
+// }
+
+
+
+// function setup() {
+//   // put setup code here
+//   canvas = createCanvas(windowWidth, windowHeight);
+//   canvas.position(0, 0);
+//   canvas.style('z-index', '-1');
+//   background('rgba(2%,20%,255%,0.1)');
+// }
+
+// function draw() {
+//   // put drawing code here
+//   if (mouseIsPressed) {
+//     strokeWeight(10);
+//     //line(pmouseX, pmouseY, mouseX, mouseY);
+//   }
+//   spray();
+// }
+
+// function spray() {
+//   if (mouseIsPressed) {
+//     for (let i = 0; i < 400; i++) {
+//       let xc = constrain(pmouseX, mouseX, windowWidth);
+//       let yc = constrain(pmouseY, mouseY, windowHeight);
+//       let x = random(-15, 15);
+//       let y = random(-15, 15);
+//       if (dist(0, 0, x, y) < 10) {
+//         ellipseMode(CENTER);
+//         ellipse(xc + x, yc + y, 1, 1);
+//       }
+//     }
+//   }
+// }
+
 var canvas;
 
 function windowResized() {
@@ -43,22 +84,23 @@ function setup() {
 function draw() {
   // put drawing code here
   if (mouseIsPressed) {
-    strokeWeight(10);
-    //line(pmouseX, pmouseY, mouseX, mouseY);
+    stroke(0, 0, 0, 70);
+
   }
   spray();
 }
 
 function spray() {
   if (mouseIsPressed) {
-    for (let i = 0; i < 400; i++) {
+    for (let i = 0; i < 2000; i++) {
       let xc = constrain(pmouseX, mouseX, windowWidth);
       let yc = constrain(pmouseY, mouseY, windowHeight);
-      let x = random(-15, 15);
-      let y = random(-15, 15);
-      if (dist(0, 0, x, y) < 10) {
+      let x = random(-50, 50);
+      let y = random(-50, 50);
+      if (dist(1, 1, x, y) < 10) {
         ellipseMode(CENTER);
-        ellipse(xc + x, yc + y, 1, 1);
+        ellipse(xc + x, yc + y, 0, 0);
+
       }
     }
   }
