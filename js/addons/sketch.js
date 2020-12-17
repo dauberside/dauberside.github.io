9fb53be5867ce245a9fb09aaa7a073b8b22d2a08
dauberside <72296037+dauberside@users.noreply.github.com>
2020-12-17 15:48:43 +0900

    Update sketch.js

--- js/addons/sketch.js
+++ js/addons/sketch.js
@@ -43,7 +43,7 @@ function setup() {
 function draw() {
   // put drawing code here
   if (mouseIsPressed) {
-    strokeWeight(1); //black
+    stroke(0, 0, 0, 100); //black
 
   }
   spray();
@@ -51,7 +51,7 @@ function draw() {
 
 function spray() {
   if (mouseIsPressed) {
-    for (let i = 0; i < 200; i++) {
+    for (let i = 0; i < 1500; i++) {
       let xc = constrain(pmouseX, mouseX, windowWidth);
       let yc = constrain(pmouseY, mouseY, windowHeight);
       let x = random(-20, 20);
@@ -61,44 +61,5 @@ function spray() {
         ellipse(xc + x, yc + y, 1, 1);
       }
     }
-  }
-}
-
-function clearBackground() {
-  fill(255); //rect color
-  rect(0, 0, width, height);
-  stroke(0); //text color
-
-}
-
-function keyTyped() {
-  if (key === 'v') {
-    stroke(148, 0, 211, 50); //violet
-  }
-  if (key === 'i') {
-    stroke(75, 0, 130, 50); //indigo
-  }
-  if (key === 'b') {
-    stroke(0, 0, 255, 50); //blue
-  }
-  if (key === 'g') {
-    stroke(0, 255, 0, 50); //green
-  }
-  if (key === 'y') {
-    stroke(255, 255, 0, 50); //yellow
-  }
-  if (key === 'o') {
-    stroke(255, 127, 0, 50); //orange
-  }
-  if (key === 'r') {
-    stroke(255, 0, 0, 50); //red
-  }
-  if (key === 'k') {
-    stroke(0, 0, 0, 70); //black
-  }
-  if (key === 'e') {
-    stroke(255); //erase
-  } else if (key === 'c') {
-    clearBackground(); //press 'c' to clear screen
   }
 }
