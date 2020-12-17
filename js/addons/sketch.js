4a0f41b5f2d7260e77238881997d91c839b85add
dauberside <72296037+dauberside@users.noreply.github.com>
2020-12-17 15:53:12 +0900

    Update sketch.js

--- js/addons/sketch.js
+++ js/addons/sketch.js
@@ -36,15 +36,15 @@ function setup() {
   // put setup code here
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
-  //canvas.style('z-index', '-1');
+  canvas.style('z-index', '-1');
   background('rgba(2%,20%,255%,0.1)');
 }
 
 function draw() {
   // put drawing code here
   if (mouseIsPressed) {
-    stroke(0, 0, 0, 70); //black
-
+    strokeWeight(15);
+    line(pmouseX, pmouseY, mouseX, mouseY);
   }
   spray();
 }
@@ -52,8 +52,8 @@ function draw() {
 function spray() {
   if (mouseIsPressed) {
     for (let i = 0; i < 400; i++) {
-      let xc = constrain(pmouseX, mouseX, windowWidth);
-      let yc = constrain(pmouseY, mouseY, windowHeight);
+      let xc = constrain(pmouseX, windowWidth);
+      let yc = constrain(pmouseY, windowHeight);
       let x = random(-20, 20);
       let y = random(-20, 20);
       if (dist(0, 0, x, y) < 20) {
