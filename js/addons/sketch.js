c3f31273b020a6613cd55133226efd19ef094d37
dauberside <72296037+dauberside@users.noreply.github.com>
2020-12-17 15:32:38 +0900

    Update sketch.js

--- js/addons/sketch.js
+++ js/addons/sketch.js
@@ -36,14 +36,14 @@ function setup() {
   // put setup code here
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
-  canvas.style('z-index', '-1');
+  //canvas.style('z-index', '-1');
   background('rgba(2%,20%,255%,0.1)');
 }
 
 function draw() {
   // put drawing code here
   if (mouseIsPressed) {
-    stroke(0, 0, 0, 70); //black
+    stroke('rgba(2%,20%,255%,0.1)'); //black
 
   }
   spray();
@@ -51,15 +51,54 @@ function draw() {
 
 function spray() {
   if (mouseIsPressed) {
-    for (let i = 0; i < 200; i++) {
+    for (let i = -10; i < 2000; i++) {
       let xc = constrain(pmouseX, mouseX, windowWidth);
       let yc = constrain(pmouseY, mouseY, windowHeight);
-      let x = random(-10, 10);
-      let y = random(-10, 10);
-      if (dist(0, 0, x, y) < 1000) {
+      let x = random(-20, 20);
+      let y = random(-20, 20);
+      if (dist(0, 0, x, y) < 20) {
         ellipseMode(CENTER);
         ellipse(xc + x, yc + y, 1, 1);
       }
     }
+  }
+}
+
+function clearBackground() {
+  fill(255); //rect color
+  rect(0, 0, width, height);
+  stroke(0); //text color
+
+}
+
+function keyTyped() {
+  if (key === 'v') {
+    stroke(148, 0, 211, 50); //violet
+  }
+  if (key === 'i') {
+    stroke(75, 0, 130, 50); //indigo
+  }
+  if (key === 'b') {
+    stroke(0, 0, 255, 50); //blue
+  }
+  if (key === 'g') {
+    stroke(0, 255, 0, 50); //green
+  }
+  if (key === 'y') {
+    stroke(255, 255, 0, 50); //yellow
+  }
+  if (key === 'o') {
+    stroke(255, 127, 0, 50); //orange
+  }
+  if (key === 'r') {
+    stroke(255, 0, 0, 50); //red
+  }
+  if (key === 'k') {
+    stroke(0, 0, 0, 70); //black
+  }
+  if (key === 'e') {
+    stroke(255); //erase
+  } else if (key === 'c') {
+    clearBackground(); //press 'c' to clear screen
   }
 }
