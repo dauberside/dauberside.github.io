c7a3205a20920642c465f2cdebc232c6baf370f4
dauberside <72296037+dauberside@users.noreply.github.com>
2020-12-28 00:36:45 +0900

    0, 0,

--- js/addons/sketch.js
+++ js/addons/sketch.js
@@ -35,6 +35,7 @@ function windowResized() {
 function setup() {
   // put setup code here
   canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
   canvas.style('z-index', '-1');
   background('rgba(2%,20%,255%,0.1)');
 }
