8253c4defc6ec935fd7dba252e3a3124ed0e5733
dauberside <72296037+dauberside@users.noreply.github.com>
2020-12-28 01:05:42 +0900

    Update sketch_jai.js

--- js/addons/sketch_jai.js
+++ js/addons/sketch_jai.js
@@ -7,13 +7,13 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-
-
 function setup() {
   // put setup code here
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
+  red = new Riso('red');
+  blue = new Riso('blue');
 }
 function draw() {
   background('rgba(2%,20%,255%,0.1)');
@@ -42,5 +42,4 @@ function draw() {
 
 function mouseClicked() {
   exportRiso();
-
 }
