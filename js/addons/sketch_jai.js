39860afc1137da00fc0114aa3e7a37b630fb8a9a
dauberside <72296037+dauberside@users.noreply.github.com>
2020-12-28 00:59:46 +0900

    Update sketch_jai.js

--- js/addons/sketch_jai.js
+++ js/addons/sketch_jai.js
@@ -14,32 +14,33 @@ function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
+}
+function draw() {
+  background('rgba(2%,20%,255%,0.1)');
+  clearRiso();
 
-  function draw() {
-    background('rgba(2%,20%,255%,0.1)');
-    clearRiso();
+  blue.fill(255);
+  blue.ellipse(200, height / 2, 300, 300);
 
-    blue.fill(255);
-    blue.ellipse(200, height / 2, 300, 300);
+  red.fill(255);
+  red.ellipse(400, height / 2, 300, 300);
 
-    red.fill(255);
-    red.ellipse(400, height / 2, 300, 300);
+  let textGraphic = createGraphics(width, height);
+  textGraphic.fill(0);
+  textGraphic.textStyle(BOLD);
+  textGraphic.textFont('Arial');
+  textGraphic.textAlign(CENTER, CENTER);
+  textGraphic.textSize(20);
+  textGraphic.text('P5.RISO', width / 2 + 5, height / 2);
 
-    let textGraphic = createGraphics(width, height);
-    textGraphic.fill(0);
-    textGraphic.textStyle(BOLD);
-    textGraphic.textFont('Arial');
-    textGraphic.textAlign(CENTER, CENTER);
-    textGraphic.textSize(20);
-    textGraphic.text('P5.RISO', width / 2 + 5, height / 2);
+  red.cutout(textGraphic); // cut text out of red
+  blue.cutout(textGraphic); // cut text out of blue
+  red.cutout(blue); // cut blue out of red
 
-    red.cutout(textGraphic); // cut text out of red
-    blue.cutout(textGraphic); // cut text out of blue
-    red.cutout(blue); // cut blue out of red
+  drawRiso();
+}
 
-    drawRiso();
-  }
+function mouseClicked() {
+  exportRiso();
 
-  function mouseClicked() {
-    exportRiso();
-  }
+}
