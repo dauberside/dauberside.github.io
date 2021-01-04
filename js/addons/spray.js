7273b3636854b212aa26acb68b642eca8165d425
dauberside <72296037+dauberside@users.noreply.github.com>
2021-01-04 18:36:10 +0900

    Update spray.js

--- js/addons/spray.js
+++ js/addons/spray.js
@@ -1,31 +1,65 @@
+// RGB color variables
+let r = 0;
+let g = 0;
+let b = 0;
 
-var color c1, c2;
-
-void setup(){
-  size(640, 640);
-  frameRate(360);
+function setup() {
+  createCanvas(windowWidth, windowHeight);
   background(255);
-  noStroke();
-  c1 = color(random(255), random(255), random(255));
-  c2 = color(random(255), random(255), random(255));
+  //setTimeout color change function
+  setTimeout(rChange, 450);
+  setTimeout(gChange, 450);
+  setTimeout(bChange, 450);
+  textSize(32);
+  text("Click to Spray!", 10, 30);
+  fill(0, 102, 153);
 }
 
-void draw(){
+// red color change
+function rChange() {
+  if (r >= 255) {
+    r = 0;
+  } else {
+    r = r + 30;
+  }
+  setTimeout(rChange, 450);
+}
 
-  if (mousePressed) {
-    for (int i = 0; i < 1000; i++) {
-      float radious = (1.0 - pow(random(1), 1.0 / 2.0)) * 100;
-      float angle = random(TWO_PI);
-      float v = 1.0 - (frameCount % 200) / 200.0;
-      color c = color(red(c1) * v + red(c2) * (1.0 - v), green(c1) * v + green(c2) * (1.0 - v), blue(c1) * v + blue(c2) * (1.0 - v));
-      fill(c, 10);
-      ellipse(mouseX + radious * cos(angle), mouseY + radious * sin(angle), 1, 1);
-    }
+// green color change
+function gChange() {
+  if (g >= 255) {
+    g = 0;
+  } else {
+    g = g + 10;
+  }
+  setTimeout(gChange, 450);
+}
 
-    if (frameCount % 200 == 199) {
-      c1 = c2;
-      c2 = color(random(255), random(255), random(255));
+// blue color change
+function bChange() {
+  if (b >= 255) {
+    b = 0;
+  } else {
+    b = b + 15;
+  }
+  setTimeout(bChange, 450);
+}
+
+// spray effect
+// TO DO
+// change squared spray to radial circle spray
+function draw() {
+  if (mouseIsPressed) {
+    for (let i = 0; i <= 1000; i++) {
+      let angle = random(TWO_PI);
+      let r = random(0, 30);
+      let offsetX = r * cos(angle);
+      let offsetY = r * sin(angle);
+      let sizeX = random(0.2, 1, 5);
+      let sizeY = random(0.2, 1, 5);
+      fill(r, g, b);
+      noStroke();
+      rect(mouseX + offsetX, mouseY + offsetY, sizeX, sizeY);
     }
   }
-
 }
