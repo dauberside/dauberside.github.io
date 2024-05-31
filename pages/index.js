012052cf461a412f931d14156451a0d16d26ba0d
DauberSide <daubersideinfo@gmail.com>
2024-05-31 11:36:58 +0900

    113

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 // pages/index.js
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = '/js/my-script.js';
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+  
   return (
     <div>
       <h1>Welcome to my site</h1>
       <p>This is a static page converted to Next.js</p>
+      <img src="/images/habit_int.gif" alt="Example Image" />
     </div>
   );
 }
