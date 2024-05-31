c4fde4c69fdff5709d3af86dc8f7a3778a30ea07
DauberSide <daubersideinfo@gmail.com>
2024-05-31 10:56:17 +0900

    11

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// pages/index.js
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my site</h1>
+      <p>This is a static page converted to Next.js</p>
+    </div>
+  );
+}
