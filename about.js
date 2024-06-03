b4157ad59ff0cb3015b8f3f5aa01fe920d7cf8af
DauberSide <daubersideinfo@gmail.com>
2024-06-04 02:31:54 +0900

    更新内容の説明

---
+++ about.js
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+
+export default function About() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>About Us</h1>
+        <p>This is the about page.</p>
+      </main>
+    </div>
+  );
+}
