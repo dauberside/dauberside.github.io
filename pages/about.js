d17ecba05953752689e10f1100b8c89dc04f585c
DauberSide <daubersideinfo@gmail.com>
2024-06-04 20:48:02 +0900

    Add about page with header and footer

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../src/components/Header';
+import Footer from '../src/components/Footer';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>About Us</h2>
+        <p>Welcome to My Website. We are dedicated to providing the best service possible.</p>
+        <p>Our mission is to deliver high-quality products that bring value to our customers.</p>
+        <p>Feel free to explore our website and learn more about what we do.</p>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default AboutPage;
