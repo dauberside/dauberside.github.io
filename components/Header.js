b0e6cd26601714dc70d1ee6f404dcbafd555faf1
DauberSide <daubersideinfo@gmail.com>
2024-06-06 13:24:27 +0900

    Update _app.js and Header.js for Bootstrap integration

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,13 @@
-// components/Header.js
 import Link from 'next/link';
 import Head from 'next/head';
-import React, { useEffect } from 'react';
 
-const Header = () => {
-  useEffect(() => {
-    // Bootstrapが必要なエフェクトがあればここに記述
-  }, []);
-
+function Header() {
   return (
     <>
       <Head>
-        <title>Contact</title>
-        <link rel="stylesheet" href="/css/crime.css" />
-        <link rel="stylesheet" href="/css/menu.css" />
-        <link rel="stylesheet" href="/css/grid.css" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/v4-shims.css" />
+        <link rel="stylesheet" href="../css/crime.css" />
+        <link rel="stylesheet" href="../css/menu.css" />
+        <link rel="stylesheet" href="../css/grid.css" />
       </Head>
       <header id="header">
         <div className="container">
@@ -27,52 +18,43 @@ const Header = () => {
               </a>
             </Link>
           </div>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul className="navbar-nav">
-                  <li className="nav-item dropdown">
-                    <Link href="#" legacyBehavior>
-                      <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Menu
-                      </a>
-                    </Link>
-                    <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                      <li>
-                        <Link href="/" legacyBehavior>
-                          <a className="dropdown-item">Home</a>
-                        </Link>
-                      </li>
-                      <li>
-                        <Link href="/project" legacyBehavior>
-                          <a className="dropdown-item">Project</a>
-                        </Link>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className="nav-item">
-                    <Link href="https://www.instagram.com/DauberSide/" legacyBehavior>
-                      <a className="nav-link"><span className="fab fa-instagram"></span></a>
+          <div className="navbar">
+            <ul className="list-inline">
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="dropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Menu
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                  <li>
+                    <Link href="/" legacyBehavior>
+                      <a className="dropdown-item">Home</a>
                     </Link>
                   </li>
-                  <li className="nav-item">
-                    <Link href="#" legacyBehavior>
-                      <a className="nav-link" data-bs-toggle="modal" data-bs-target="#squarespaceModal">
-                        <span className="far fa-envelope"></span>
-                      </a>
+                  <li>
+                    <Link href="/project" legacyBehavior>
+                      <a className="dropdown-item">Project</a>
                     </Link>
                   </li>
                 </ul>
-              </div>
-            </div>
-          </nav>
+              </li>
+              <li>
+                <Link href="https://www.instagram.com/DauberSide/" legacyBehavior>
+                  <a><span className="fab fa-instagram"></span></a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#" legacyBehavior>
+                  <a data-toggle="modal" data-target="#squarespaceModal">
+                    <span className="far fa-envelope"></span>
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </header>
     </>
   );
-};
+}
 
 export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 // pages/_app.js
-import Script from 'next/script';
 import '../src/styles/global.css';
+import Script from 'next/script';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/v4-shims.css" />
+      </Head>
       <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         strategy="lazyOnload"
       />
       <Component {...pageProps} />
