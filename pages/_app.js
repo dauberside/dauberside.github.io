d1b03520992be9972905dd4a049d4080ddf5523b
DauberSide <daubersideinfo@gmail.com>
2024-06-09 07:54:29 +0900

    ContactForm

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,31 @@
 import '../src/styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Script from 'next/script';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      document.addEventListener('DOMContentLoaded', function () {
+        const exampleModal = document.getElementById('exampleModal');
+        if (exampleModal) {
+          exampleModal.addEventListener('show.bs.modal', event => {
+            const button = event.relatedTarget;
+            const recipient = button.getAttribute('data-bs-whatever');
+            const modalTitle = exampleModal.querySelector('.modal-title');
+            const modalBodyInput = exampleModal.querySelector('.modal-body input');
+
+            modalTitle.textContent = `New message to ${recipient}`;
+            modalBodyInput.value = recipient;
+          });
+        }
+      });
+    }
+  }, []);
+
   return (
     <>
-      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" strategy="lazyOnload" />
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" strategy="beforeInteractive" />
       <Component {...pageProps} />
     </>
   );
