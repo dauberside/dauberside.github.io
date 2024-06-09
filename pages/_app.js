e8aabc5d4b51186a7dc3ed6e8f188d556cc157fe
DauberSide <daubersideinfo@gmail.com>
2024-06-09 17:48:03 +0900

    ContactForm

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,32 @@
+import '../src/styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect } from 'react';
-
-const initializeModal = () => {
-  const exampleModal = document.getElementById('exampleModal');
-  if (exampleModal) {
-    exampleModal.addEventListener('show.bs.modal', event => {
-      const button = event.relatedTarget;
-      const recipient = button.getAttribute('data-bs-whatever');
-      const modalTitle = exampleModal.querySelector('.modal-title');
-      const modalBodyInput = exampleModal.querySelector('.modal-body input');
-
-      modalTitle.textContent = `New message to ${recipient}`;
-      modalBodyInput.value = recipient;
-    });
-  }
-};
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    import('bootstrap').then(() => {
-      initializeModal();
-    });
+    const loadBootstrap = async () => {
+      const script = document.createElement('script');
+      script.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js';
+      script.async = true;
+      script.onload = () => {
+        const exampleModal = document.getElementById('exampleModal');
+        if (exampleModal) {
+          exampleModal.addEventListener('show.bs.modal', event => {
+            const button = event.relatedTarget;
+            const recipient = button.getAttribute('data-bs-whatever');
+            const modalTitle = exampleModal.querySelector('.modal-title');
+            const modalBodyInput = exampleModal.querySelector('.modal-body input');
+            modalTitle.textContent = `New message to ${recipient}`;
+            modalBodyInput.value = recipient;
+          });
+        }
+      };
+      document.body.appendChild(script);
+    };
+    loadBootstrap();
   }, []);
 
-  return (
-    <>
-      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" strategy="beforeInteractive" />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
