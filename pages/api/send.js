e8cfe94eead56ef79e78a9b83309a68395c4ac47
DauberSide <daubersideinfo@gmail.com>
2024-06-09 22:43:48 +0900

    ContactForm

--- pages/api/send.js
+++ pages/api/send.js
@@ -9,7 +9,7 @@ export default async (req, res) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: true, // true for 465, false for other ports
+    secure: process.env.SMTP_PORT === '465', // true for 465, false for other ports
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
@@ -17,7 +17,7 @@ export default async (req, res) => {
   });
 
   const mailOptions = {
-    from: email,
+    from: process.env.SMTP_USER,
     to: process.env.SMTP_USER,
     subject: `Contact request from ${name}`,
     text: message,
