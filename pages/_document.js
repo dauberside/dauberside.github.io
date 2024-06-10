dd01354cb04884d857d28c83dd0854a3472078d8
DauberSide <daubersideinfo@gmail.com>
2024-06-10 23:05:21 +0900

    Update deploy workflow

--- pages/_document.js
+++ pages/_document.js
@@ -31,6 +31,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
         </body>
       </Html>
