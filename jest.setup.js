ba0f08f7d68b5d54d8613c379473dee61226a4a1
DauberSide <daubersideinfo@gmail.com>
2024-06-11 19:41:01 +0900

    Clean up project files and update dependencies

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,3 @@
-import { TextEncoder, TextDecoder } from 'util';
+import '@testing-library/jest-dom/extend-expect';
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
-import '@testing-library/jest-dom/extend-expect';
