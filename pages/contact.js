97bb00ee3f901dad6d893635f8be666daf7cc041
DauberSide <daubersideinfo@gmail.com>
2024-06-11 19:58:11 +0900

    Clean up project files and update dependencies

--- pages/contact.js
+++ pages/contact.js
@@ -1,19 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import React from 'react';
+import ContactForm from '../components/ContactForm';
 
-const Contact = () => {
-  useEffect(() => {
-  }, []);
-
+const ContactPage = () => {
   return (
     <div>
-      <Header />
       <h1>Contact Us</h1>
       <ContactForm />
-      <Footer />
     </div>
   );
 };
 
-export default Contact;
+export default ContactPage;
