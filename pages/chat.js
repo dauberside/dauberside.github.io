1f84671b03e3031a8bda5c82814b9daaf44dbded
DauberSide <daubersideinfo@gmail.com>
2024-06-12 02:00:17 +0900

    Clean up project files and update dependencies

--- pages/chat.js
+++ pages/chat.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import supabase from '../src/utils/supabaseClient.js'; // 正しいパスを確認
+import supabase from '../src/utils/supabaseClient.js';
 
 const Chat = () => {
   const [messages, setMessages] = useState([]);
