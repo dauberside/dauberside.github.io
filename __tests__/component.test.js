0e591987fe320ec79c9d67235f054fe33626ad2e
DauberSide <daubersideinfo@gmail.com>
2024-06-11 19:19:01 +0900

    Clean up project files and update dependencies

--- __tests__/component.test.js
+++
@@ -1,9 +0,0 @@
-// __tests__/component.test.js
-import { render, screen } from '@testing-library/react';
-import Component from '../components/Component';
-
-test('renders component', () => {
-  render(<Component />);
-  const linkElement = screen.getByText(/some text/i);
-  expect(linkElement).toBeInTheDocument();
-});
