2917c072e9ea3ae96f5b64aeca6201cc09ca82cd
DauberSide <daubersideinfo@gmail.com>
2024-06-13 19:34:49 +0900

    Remove .env.local and add to .gitignore

--- next.config.js
+++ next.config.js
@@ -3,16 +3,16 @@ module.exports = {
     return [
       {
         source: '/socket.io/:path*',
-        destination: '/api/socket',
+        destination: '/api/socket/:path*',
       },
     ];
   },
   env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
     SMTP_HOST: process.env.SMTP_HOST,
     SMTP_PORT: process.env.SMTP_PORT,
     SMTP_USER: process.env.SMTP_USER,
     SMTP_PASS: process.env.SMTP_PASS,
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
   },
 };
