51812199224fb7a47028bac5ee3aa088e8d74613
DauberSide <daubersideinfo@gmail.com>
2024-06-13 14:18:49 +0900

    Initial commit

--- next.config.js
+++ next.config.js
@@ -8,11 +8,11 @@ module.exports = {
     ];
   },
   env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
     SMTP_HOST: process.env.SMTP_HOST,
     SMTP_PORT: process.env.SMTP_PORT,
     SMTP_USER: process.env.SMTP_USER,
     SMTP_PASS: process.env.SMTP_PASS,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
   },
 };
--- pages/chat.js
+++ pages/chat.js
@@ -1,65 +1,77 @@
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import { io } from 'socket.io-client';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import supabase from '../src/utils/supabaseClient.js';
+import supabase from '../src/utils/supabaseClient';
+
+const socket = io({
+  path: '/socket.io',
+});
 
 const Chat = () => {
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState('');
   const [username, setUsername] = useState('');
-  const [userId, setUserId] = useState(null);
+  const [userId, setUserId] = useState('');
+  const router = useRouter();
 
   useEffect(() => {
     const fetchMessages = async () => {
-      const { data, error } = await supabase
-        .from('messages')
-        .select('*')
-        .order('created_at', { ascending: true });
+      try {
+        const response = await fetch('/api/messages');
+        const data = await response.json();
 
-      if (error) {
-        console.error('Error fetching messages:', error.message);
-      } else {
-        setMessages(data);
-      }
-    };
-
-    const fetchUserId = async () => {
-      const user = supabase.auth.user();
-      if (user) {
-        setUserId(user.id);
-      } else {
-        console.error('User not authenticated');
+        if (response.ok) {
+          setMessages(data);
+        } else {
+          console.error('Error fetching messages:', data.error);
+        }
+      } catch (error) {
+        console.error('Unexpected error fetching messages:', error);
       }
     };
 
     fetchMessages();
-    fetchUserId();
-
-    const socket = io({
-      path: '/socket.io',
-    });
-
-    socket.on('connect', () => {
-      console.log('connected');
-    });
-
-    socket.on('init', (data) => {
-      setMessages(data);
-    });
 
     socket.on('chat message', (msg) => {
       setMessages((prevMessages) => [...prevMessages, msg]);
     });
 
     return () => {
-      socket.disconnect();
+      socket.off('chat message');
     };
   }, []);
 
+  useEffect(() => {
+    const fetchUserId = async () => {
+      try {
+        const { data, error } = await supabase.auth.getUser();
+        console.log('Fetched user data:', data);
+        if (error) {
+          console.error('Error fetching user ID:', error);
+        } else if (data && data.user) {
+          setUserId(data.user.id);
+        } else {
+          console.error('No user found');
+          router.push('/login'); // ログインページにリダイレクト
+        }
+      } catch (error) {
+        console.error('Unexpected error fetching user ID:', error);
+      }
+    };
+
+    fetchUserId();
+  }, [router]);
+
   const handleSendMessage = async () => {
-    try {
-      if (message.trim() && username.trim() && userId) {
+    if (!userId) {
+      console.error('User ID is not set');
+      return;
+    }
+
+    if (message.trim() && username.trim()) {
+      try {
         const response = await fetch('/api/messages', {
           method: 'POST',
           headers: {
@@ -68,54 +80,59 @@ const Chat = () => {
           body: JSON.stringify({ user_id: userId, username, text: message }),
         });
 
+        const data = await response.json();
+
         if (response.ok) {
-          const data = await response.json();
+          socket.emit('chat message', data[0]);
           setMessages((prevMessages) => [...prevMessages, data[0]]);
           setMessage('');
         } else {
-          const errorData = await response.json();
-          console.error('Failed to send message:', errorData);
+          console.error('Failed to send message:', data.error);
         }
+      } catch (error) {
+        console.error('Unexpected error sending message:', error);
       }
-    } catch (error) {
-      console.error('Unexpected error:', error);
+    } else {
+      console.error('Username or message is empty');
     }
   };
 
   return (
     <div>
       <Header />
-      <main className="container mx-auto p-4">
-        <div className="bg-white p-6 rounded-lg shadow-md">
-          <div className="chat-box overflow-auto h-80 mb-4 border p-4">
-            {messages.map((msg, index) => (
-              <div key={index} className="chat-message p-2 border-b">
-                <strong>{msg.username}:</strong> {msg.text}
-              </div>
-            ))}
-          </div>
-          <div className="chat-input">
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Your name"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="mb-2 p-2 border rounded w-full"
-            />
-            <input
-              type="text"
-              id="text"
-              name="text"
-              placeholder="Type a message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              className="mb-2 p-2 border rounded w-full"
-            />
-            <button onClick={handleSendMessage} className="w-full bg-blue-500 text-white py-2 rounded">
-              Send
-            </button>
+      <main>
+        <div className="container my-5">
+          <div className="chat-container">
+            <div className="chat-box">
+              {messages.map((msg, index) => (
+                <div key={index} className="chat-message">
+                  <strong>{msg.username}:</strong> {msg.text}
+                </div>
+              ))}
+            </div>
+            <div className="chat-input">
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="form-control mb-2"
+                placeholder="Your name"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <input
+                type="text"
+                id="text"
+                name="text"
+                className="form-control mb-2"
+                placeholder="Type a message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <button className="btn btn-primary" onClick={handleSendMessage}>
+                Send
+              </button>
+            </div>
           </div>
         </div>
       </main>
