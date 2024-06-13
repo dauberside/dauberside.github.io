8809e27449ca192901a3094a892bb9e04cbb1141
DauberSide <daubersideinfo@gmail.com>
2024-06-13 14:24:41 +0900

    Initial commit

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Securely load environment variables
-const supabaseUrl: string | undefined = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey: string | undefined = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing Supabase URL or Key in environment variables");
