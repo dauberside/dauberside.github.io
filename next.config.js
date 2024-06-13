5426c1cc998407d768e212e8724956443c13e9cc
DauberSide <daubersideinfo@gmail.com>
2024-06-13 11:54:51 +0900

    Initial commit

--- next.config.js
+++ next.config.js
@@ -12,8 +12,8 @@ module.exports = {
       SMTP_PORT: process.env.SMTP_PORT,
       SMTP_USER: process.env.SMTP_USER,
       SMTP_PASS: process.env.SMTP_PASS,
-      SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-      SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
+      NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+      NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
     },
   };
   
