736cb6e4a81f60012434defc80cf1885f42e3c1b
DauberSide <daubersideinfo@gmail.com>
2025-07-26 13:44:50 +0900

    メッセージ送れない

--- src/pages/dauber.tsx
+++ src/pages/dauber.tsx
@@ -30,21 +30,21 @@ const Dauber: React.FC = () => {
     jp: {
       title: "Dauber",
       description: [
-        "2004年スウェーデンのA5pressからKingsize DIY marker & globalization of Graffitiと題された書籍が発行された。これは私が知る限りDIYマーカーとその背景について記録された唯一の書籍である。",
-        "DIYマーカーとは、グラフィティを行うライターたちが路上でタギング(サイン)の個性を競う経緯を経て制作されるようになったツールで、彼らは既存のペンやマーカーの改造、靴磨きや車用のポリッシャーのような別用途である製品の流用、自主制作等を行い、それぞれの自己流マーカーを作るという行為を行ってきた。",
-        "イギリスから生まれ、1970年代に北米を中心に広がりを見せたDIYムーブメント(Do it Yourself運動)と時を同じくしてニューヨークで生まれたグラフィティにとってこうした流れはごく自然なことであった。だがこうした技術や資料は、グラフィティの広がるスピードとは反対に、匿名性やクルー(組織集団)というグラフィティがもつ性質によって、一部の共同体の中でしかシェアされることがなかったため、インターネットが普及する近年まで書籍や情報として全体に共有されることが非常に少なかったと言える。",
-        "Kingsizeは、その書籍の名のごとく大きなマーカーを制作する一連の過程といくつかの論述で構成されている。当時としては見ることがなかったであろう50mmの太さのマーカーを、資材量販店で揃えたであろう素材を組み合わせて制作する過程と、そのマーカーを世界中のライターたちに配布、実用している記録写真。そしてkingsizeの発行者であり作家のアダムスの序文、ニューヨークで初期にグラフィティライターの青年たちへのインタビューを行い書籍[getting up]を著したクレイグ・キャッスルマンによる当時の情景描写、世界大戦後グローバリズムが進む中で世界中にアメリカナイゼーションが浸透していく様子と、その影響を受けるスウェーデン社会を捉えたアンドレアス・バーグの論述で構成されていた。",
-        "先にも述べたように匿名性やクルーという性質を持つグラフィティカルチャーは、しばしばその細かな流れや背景等が、記録からもれ抜け落ちてしまうことが多い。そうした意味で世界のグローバリズムの中でグラフィティがニューヨークを経て世界中に広がっていく風景と、スウェーデンでの背景や流れを後世に残る形でドキュメントしていたこの書籍の役割は大きい。",
-        "ではkingsizeが出版されてから16年が経過した今、ストリートで活動を続けるライターたちにとってのDIYマーカー、ツールどう変化したのだろうか。このプロジェクトでは、グラフィティにおけるツールの変容と表現の変容を考察するとともに、kingsizeを参照としながら、2010年代以降のメーカームーブメントの発展とともに可能となったデジタルファブリケーションの技術をもとに3Dソフトによるモデリングや、ソーシャルネットワーキングによる情報共有を試み、現代におけるDIYマーカーのあり方を探る。",
+          "2004年スウェーデンのA5pressからKingsize DIY marker & globalization of Graffitiと題された書籍が発行された。これは私が知る限りDIYマーカーとその背景について記録された唯一の書籍である。",
+          "DIYマーカーとは、グラフィティを行うライターたちが路上でタギング(サイン)の個性を競う経緯を経て制作されるようになったツールで、彼らは既存のペンやマーカーの改造、靴磨きや車用のポリッシャーのような別用途である製品の流用、自主制作等を行い、それぞれの自己流マーカーを作るという行為を行ってきた。",
+          "イギリスから生まれ、1970年代に北米を中心に広がりを見せたDIYムーブメント(Do it Yourself運動)と時を同じくしてニューヨークで生まれたグラフィティにとってこうした流れはごく自然なことであった。だがこうした技術や資料は、グラフィティの広がるスピードとは反対に、匿名性やクルー(組織集団)というグラフィティがもつ性質によって、一部の共同体の中でしかシェアされることがなかったため、インターネットが普及する近年まで書籍や情報として全体に共有されることが非常に少なかったと言える。",
+          "Kingsizeは、その書籍の名のごとく大きなマーカーを制作する一連の過程といくつかの論述で構成されている。当時としては見ることがなかったであろう50mmの太さのマーカーを、資材量販店で揃えたであろう素材を組み合わせて制作する過程と、そのマーカーを世界中のライターたちに配布、実用している記録写真。そしてkingsizeの発行者であり作家のアダムスの序文、ニューヨークで初期にグラフィティライターの青年たちへのインタビューを行い書籍[getting up]を著したクレイグ・キャッスルマンによる当時の情景描写、世界大戦後グローバリズムが進む中で世界中にアメリカナイゼーションが浸透していく様子と、その影響を受けるスウェーデン社会を捉えたアンドレアス・バーグの論述で構成されていた。",
+          "先にも述べたように匿名性やクルーという性質を持つグラフィティカルチャーは、しばしばその細かな流れや背景等が、記録からもれ抜け落ちてしまうことが多い。そうした意味で世界のグローバリズムの中でグラフィティがニューヨークを経て世界中に広がっていく風景と、スウェーデンでの背景や流れを後世に残る形でドキュメントしていたこの書籍の役割は大きい。",
+          "ではkingsizeが出版されてから16年が経過した今、ストリートで活動を続けるライターたちにとってのDIYマーカー、ツールどう変化したのだろうか。このプロジェクトでは、グラフィティにおけるツールの変容と表現の変容を考察するとともに、kingsizeを参照としながら、2010年代以降のメーカームーブメントの発展とともに可能となったデジタルファブリケーションの技術をもとに3Dソフトによるモデリングや、ソーシャルネットワーキングによる情報共有を試み、現代におけるDIYマーカーのあり方を探る。",
       ],
     },
     en: {
       title: "Dauber",
       description: [
-        'In 2004, A book entitled "KING SIZE A project about Tags, DIY-Craft & Subcultural Globalization" was published by A5press in Sweden. This is the only book I know about DIY markers and their background.',
+        "In 2004, A book entitled KING SIZE A project about Tags, DIY-Craft & Subcultural Globalization was published by A5press in Sweden. This is the only book I know about DIY markers and their background.",
         "DIY markers are tools that graffiti writers has come to create while competing for the individuality of tagging(signatures) on the street. They have modified existing pens and markers, diverted products for other purposes such as shoe polishers and for cars, etc. and made their own self made markers.",
         "This flow was quite natural for graffiti culture, which was born in New York at the same time as the DIY movement that was born in England and spread mainly in North America in the 1970s. However, contrary to the speed at which graffiti spreads these technologies and materials were spread only within some small communities due to the nature of graffiti such as anonymity and crew, so until the internet became widespread at recent years that it could be said that it was not a lot shared as a book or information.",
-        'Kingsize consists of a series of processes and several essays that create a large marker, as the name of the book suggests. by combining materials that would have been prepared at a material mass retailer, the process of producing a marker with a thickness off 50mm, which would not have been seen at that time. recorded photos of distributing the marker to writers all over the world for practical use. the preface by publisher of King size and artist Adams. the scene description of the early days by Craig Cattleman who wrote the book "getting up" by interviewing young graffiti writers in New York. And Andreas Berg\'s essay on how Americanization pervades the world as globalism progresses after World War 2, and the Swedish society affected by it.',
+        "Kingsize consists of a series of processes and several essays that create a large marker, as the name of the book suggests. by combining materials that would have been prepared at a material mass retailer, the process of producing a marker with a thickness off 50mm, which would not have been seen at that time. recorded photos of distributing the marker to writers all over the world for practical use. the preface by publisher of King size and artist Adams. the scene description of the early days by Craig Cattleman who wrote the book getting up by interviewing young graffiti writers in New York. And Andreas Berg's essay on how Americanization pervades the world as globalism progresses after World War 2, and the Swedish society affected by it.",
         "as mentioned earlier, graffiti culture which has the characteristics of anonymity and crew, often loses its detailed flow and background from the records. in that sense, the role of this book which documented the landscape of graffiti spreading all over the world through New York while the globalism of the world, and the background and flow of Sweden in a way that will remain in posterity is significant.",
         "Now, 16 years ofter Kingsize was published, how have DIY makers and tools changed for writers who continue to writing on the street. In this project, we will consider the transformation of tools and expressions in graffiti, and while referring to kingsize, modeling with 3D software based on the digital fabrication technology that became possible with the development of the maker movement after the 2010s. And we will try to share information and file by social networking and explore the ideal way of DIY makers in modern times.",
       ],
@@ -66,14 +66,14 @@ const Dauber: React.FC = () => {
                 Language:
                 <button
                   onClick={() => setLanguage("jp")}
-                  className={`ml-2 ${language === "jp" ? "text-blue-400 underline" : "text-white"}`}
+                  className={`language-button ${language === "jp" ? "active" : ""}`}
                 >
                   JP
                 </button>
                 /
                 <button
                   onClick={() => setLanguage("en")}
-                  className={`${language === "en" ? "text-blue-400 underline" : "text-white"}`}
+                  className={`language-button ${language === "en" ? "active" : ""}`}
                 >
                   EN
                 </button>
